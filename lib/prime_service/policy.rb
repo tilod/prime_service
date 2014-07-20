ac56e81d138fa8933a575eb45781f4793b127c6d
Tilo Dietrich <tilodietrich@posteo.de>
2014-07-20 23:20:50 +0200

    add .option and .flag macro methods to Policy

--- lib/prime_service/policy.rb
+++ lib/prime_service/policy.rb
@@ -1,9 +1,7 @@
 module PrimeService
   class Policy
     def self.call_with(*call_params)
-      define_method :_call_params_ do
-        call_params
-      end
+      _definition_.args = call_params
 
       call_params.each do |param|
         attr_accessor param
@@ -11,17 +9,60 @@ module PrimeService
     end
 
 
-    def initialize(*params)
-      _call_params_.each_with_index do |attribute, index|
-        instance_variable_set "@#{attribute}", params[index]
+    def self.option(option_name, option_default = nil)
+      _definition_.options[option_name] = option_default
+
+      attr_accessor option_name
+    end
+
+
+    def self.flag(flag_name, flag_default = false)
+      _definition_.flags[flag_name] = flag_default
+
+      define_method "#{flag_name}?" do
+        !!(instance_variable_get "@#{flag_name}")
+      end
+      attr_accessor flag_name
+    end
+
+
+    def initialize(*args)
+      definition = self.class._definition_
+
+      definition.args.each_with_index do |attribute, index|
+        instance_variable_set "@#{attribute}", args[index]
+      end
+
+      if args.size > definition.args.size && args.last.class == Hash
+        options_hash = args.last
+
+        definition.options.each do |option_name, option_default|
+          instance_variable_set "@#{option_name}",
+                                 options_hash[option_name] || option_default
+        end
+
+        definition.flags.each do |flag_name, flag_default|
+          instance_variable_set "@#{flag_name}",
+                                 options_hash[flag_name] || flag_default
+        end
       end
     end
 
 
     private
 
-    def _call_params_
-      []
+    def self._definition_
+      @_definition_ ||= Definition.new
+    end
+
+    class Definition
+      attr_accessor :args, :options, :flags
+
+      def initialize
+        @args    = []
+        @options = {}
+        @flags   = {}
+      end
     end
   end
 end
--- spec/prime_service/policy_spec.rb
+++ spec/prime_service/policy_spec.rb
@@ -10,25 +10,38 @@ module PrimeService
   end
 
 
+  class TestPolicyWithOptionsAndFlags < Policy
+    call_with :foo
+
+    option    :option_1
+    option    :option_2, "default value"
+    option    :option_3
+
+    flag      :flag_1
+    flag      :flag_2, true
+    flag      :flag_3
+  end
+
+
   describe Policy do
     let(:test_policy) { TestPolicy.new("foo value", "bar value") }
 
 
-    describe "policy with initializer params" do
+    describe "policy with initializer arguments" do
       describe ".call_with" do
-        it "defines attribute readers for the call params" do
+        it "defines attribute readers for the call arguments" do
           expect(test_policy).to respond_to :foo
           expect(test_policy).to respond_to :bar
         end
 
-        it "defines attribute writers for the call params" do
+        it "defines attribute writers for the call arguments" do
           test_policy.foo = "new foo"
           expect(test_policy.foo).to eq "new foo"
         end
       end
 
       describe "#initialize" do
-        it "assigns the params to instance variables" do
+        it "assigns the arguments to instance variables" do
           expect(test_policy.foo).to eq "foo value"
           expect(test_policy.bar).to eq "bar value"
         end
@@ -36,11 +49,79 @@ module PrimeService
     end
 
 
-    describe "policy without initializer params" do
+    describe "policy without initializer arguments" do
       let(:test_policy) { TestPolicyWithoutParams.new }
 
       it "works" do
         expect(test_policy).to be_a Policy
+      end
+    end
+
+
+    describe "policy with options and flags" do
+      let(:test_policy) {
+        TestPolicyWithOptionsAndFlags.new("foo value",
+                                           option_1: "option 1 argument",
+                                           flag_1:    true)
+      }
+
+      describe ".option" do
+        it "defines attribute reader for the options" do
+          expect(test_policy).to respond_to :option_1
+          expect(test_policy).to respond_to :option_2
+          expect(test_policy).to respond_to :option_3
+        end
+
+        it "defines attribute writers for the options" do
+          test_policy.option_1 = "new value"
+          expect(test_policy.option_1).to eq "new value"
+        end
+
+        it "uses `nil` as default value for the options when no default value "\
+           "is defined" do
+          expect(test_policy.option_3).to be_nil
+        end
+      end
+
+      describe "#initialize" do
+        it "assigns the option arguments to the options" do
+          expect(test_policy.option_1).to eq "option 1 argument"
+        end
+
+        it "it keeps the default value in place when no value for an option "\
+           "is passed" do
+          expect(test_policy.option_2).to eq "default value"
+        end
+      end
+
+
+      describe ".flag" do
+        it "defines attribute reader with question marks for the flags" do
+          expect(test_policy).to respond_to :flag_1?
+          expect(test_policy).to respond_to :flag_2?
+          expect(test_policy).to respond_to :flag_3?
+        end
+
+        it "defines attribute writers for the flags" do
+          test_policy.flag_3 = :truthy
+          expect(test_policy.flag_3?).to be true
+        end
+
+        it "uses `false` as default value for the flags when no default value "\
+           "is defined" do
+          expect(test_policy.flag_3?).to be false
+        end
+      end
+
+      describe "#initialize" do
+        it "assigns the flag arguments to the flags" do
+          expect(test_policy.flag_1?).to be true
+        end
+
+        it "it keeps the default value in place when no value for an flag "\
+           "is passed" do
+          expect(test_policy.flag_2?).to be true
+        end
       end
     end
   end
